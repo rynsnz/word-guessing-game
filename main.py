3e196ffe738802e51603c161b9d9dbaf2157bbbc
Ryan Saenz-Kelleher <36112399+rynsnz@users.noreply.github.com>
2022-11-20 16:01:06 -0600

    Commit

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ winning = list(chosen_word.strip())
 # establish a prompt for the length of the word with spaces
 prompt = ['_' for n in chosen_word]
 
-# Ask the user to guess a letter and assign their answer to a variable
-print(f"Psst... the word is {chosen_word}")
-
 # Game in play
 chances = ['1', '2', '3', '4', '5', '6']
 while prompt != winning:
