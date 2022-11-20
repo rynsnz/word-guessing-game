02f6bf9df963de911c7f3593bd58f17313c865bc
Ryan Saenz-Kelleher <36112399+rynsnz@users.noreply.github.com>
2022-11-20 15:16:29 -0600

    Check if the player won

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import random
 word_list = ["apple", "orange", "banana"]
 
+'''
+Set up the game
+'''
 # randomly choose a word from the word_list and assign it to a variable
 # chosen_word = word_list[random.randint(0, len(word_list) - 1)]
 chosen_word = random.choice(word_list)
+
+# establish a comparison list
+winning = list(chosen_word.strip())
 
 # establish a prompt for the length of the word with spaces
 prompt = ['_' for n in chosen_word]
@@ -11,13 +17,18 @@ prompt = ['_' for n in chosen_word]
 # Ask the user to guess a letter and assign their answer to a variable
 print(f"Psst... the word is {chosen_word}")
 
-guess = input("Guess a letter: ").lower()
+# Game in play
+while prompt != winning:
+    guess = input("Guess a letter: ").lower()
 
-# is the letter in the random word?
-# guess in chosen_word
+    '''
+    Check to see if the letter in the random word?
+    If true, replace the guessed letter in the prompt
+    '''
+    for position in range(len(chosen_word)):
+        if guess == chosen_word[position]:
+            prompt[position] = guess
 
-for position in range(len(chosen_word)):
-    if guess == chosen_word[position]:
-        prompt[position] = guess
+    print(prompt)
 
-print(prompt)
+print('You won!')
