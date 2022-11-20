21a471d3941841b7aed73a37f3438b6a0fa00b21
Ryan Saenz-Kelleher <36112399+rynsnz@users.noreply.github.com>
2022-11-20 14:25:47 -0600

    First commit

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import random
 word_list = ["dog", "cat", "horse"]
 
-'''
-randomly choose a word from the word_list and assign it to a variable
-called chosen_word
-'''
-chosen_word = word_list[random.randint(0, len(word_list) - 1)]
+# randomly choose a word from the word_list and assign it to a variable
+# chosen_word = word_list[random.randint(0, len(word_list) - 1)]
+chosen_word = random.choice(word_list)
 
-'''
-Ask the user to guess a letter and assign their answer to a variable
-called guess. Make guess lowercase.
-'''
+# Ask the user to guess a letter and assign their answer to a variable
 guess = input("Guess a letter: ").lower()
 
-'''
-Check if the letter the user guessed (guess) is one of the letters
-in the chosen_word
-'''
-guess in chosen_word
+# is the letter in the random word?
+# guess in chosen_word
+for letter in chosen_word:
+    if letter in guess:
+        print("Right")
+    else:
+        print("Wrong")
